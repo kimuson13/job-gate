fc3e8ee52472f30c0280f620b3982c41a38f25bd
kimuson <kimuson13@basket.com>
2021-08-28 10:04:14 +0900

    小学校編までは行けるようにしました

--- app/models.py
+++ app/models.py
@@ -3,11 +3,20 @@ from django.conf import settings
 from django.utils import timezone
 
 
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField("タイトル", max_length=200)
-    content = models.TextField('本文')
-    created = models.DateField('作成日', default=timezone.now)
+# class Post(models.Model):
+#     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     title = models.CharField("タイトル", max_length=200)
+#     content = models.TextField('本文')
+#     created = models.DateField('作成日', default=timezone.now)
+
+#     def __str__(self):
+#         return self.title
+
+class User(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
 
     def __str__(self):
-        return self.title
+        return '('+self.id+')', self.name
+
--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
-    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
-    path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post_edit'),
-    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
-
+    path('home', views.homeView.as_view(), name='home'),
+    path('home/question-description', views.QuestionDescriptionView.as_view(), name='question_description'),
+    path('home/elementary-school', views.ElementarySchoolView.as_view(), name='elementary-school'),
+    path('home/junior-high-school', views.JuniorHighSchoolView.as_view(), name='junior-high-school'),
+    path('home/high-school', views.HighSchoolView.as_view(), name='high-school'),
+    path('home/university', views.UniversityView.as_view(), name='university'),
+    # path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
+    # path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    # path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post_edit'),
+    # path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
